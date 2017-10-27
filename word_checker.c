8e6c65cedfc9f308aef595f4565f452220180de4
Mayez <31999530+Mayez@users.noreply.github.com>
2017-10-27 15:28:02 -0400

    Update word_checker.c

--- word_checker.c
+++ word_checker.c
@@ -7,11 +7,9 @@
   Description: This function will get the letter
                stored in a specific place in the
                structure caled rolledDice.
-  Parameters: int i, int j, struct rolledDice **gameBoard
-  Return: gameBoard[i][i].character
 */
 char getLetter(int i, int j, struct rolledDice **gameBoard) {
-  return gameBoard[i][j].character;
+	return gameBoard[i][j].character;
 }
 
 /*
@@ -19,55 +17,49 @@ char getLetter(int i, int j, struct rolledDice **gameBoard) {
                the word inputted by the user during the game
                will meet all of the requirements in order
                for them to achieve a point
-  Parameters: int i, int j, char *word, struct rolledDice **gameBoard, int subLen, int **visited
-  Return: 0 or 1
 */
 int abidesRules(int i, int j, char *word, struct rolledDice **gameBoard, int subLen, int **visited) {
-
-  int ans = (subLen == (strlen(word)-1));
-  int allX[] = {0, -1, -1, -1, 0, 1, 1, 1};
+	int ans = (subLen == (strlen(word)-1));
+	int allX[] = {0, -1, -1, -1, 0, 1, 1, 1};
 	int allY[] = {1, 1, 0, -1, -1, -1, 0, 1};
 	int newX = 0;
 	int newY = 0;
-  int result = 0;
-  char currentLetter = word[subLen];
-  char findLetter = '';
+	int result = 0;
+	char currentLetter = word[subLen];
+	char findLetter = '';
 
-  if(subLen == (strlen(word)-1)) {
-    return 1;
+	if(subLen == (strlen(word)-1)) {
+	    return 1;
 	}
 
 	// right, digUpRight, up, digUpLeft, left, digDownLeft, down, digDownRight
 
-  findLetter = word[subLen + 1];
-
+	findLetter = word[subLen + 1];
 
 	for(int adjCell = 0; adjCell < 8; adjCell++) {
 	    int newX = i + allX[adjCell];
 	    int newY = j + allY[adjCell];
-			if((newX >= 0) && (newX < 4) && (newY >=0) && (newY < 4) && toupper(findLetter) == get_Letter(newX, newX, gameBoard) && !visited[newX][newY]) {
-			    visited[newX][newY] = 1;
-					++subLen;
-					result = abidesRules(newX, newY, word, gameBoard, subLen, visited);
-		  if(result) {
-			    return 1;
-			}else {
-			    --subLen;
+		if((newX >= 0) && (newX < 4) && (newY >=0) && (newY < 4) && toupper(findLetter) == get_Letter(newX, newX, gameBoard) && !visited[newX][newY]) {
+		    visited[newX][newY] = 1;
+			++subLen;
+			result = abidesRules(newX, newY, word, gameBoard, subLen, visited);
+			if(result) {
+				return 1;
 			}
-
-				}
+			else {
+				--subLen;
+			}
 
 		}
 
-		return 0;
+	}
+	return 0;
 }
 
 /*
   Description: This function will check to see
                if the word entered by the user
                actually exists in the game
-  Parameters: struct rolledDice **gameBoard, char *word
-  Return: 0 or 1
 */
 int wordChecker(struct rolledDice **gameBoard, char *word) {
 
@@ -80,26 +72,27 @@ int wordChecker(struct rolledDice **gameBoard, char *word) {
 	visited[3] = malloc(sizeof(int) * 4);
 
 	for(int m = 0; m < 4; m++) {
-	   for(int n = 0; n < 4; n++) {
-		     if(m == 0 && n == 0) {
-			   visited[m][n] = 1;
-			   }else {
-			   visited[m][n] = 0;
-			   }
-		  }
+		for(int n = 0; n < 4; n++) {
+		    if(m == 0 && n == 0) {
+			    visited[m][n] = 1;
+			}
+		   	else {
+			    visited[m][n] = 0;
+			}
+		}
 	}
 
 	for (int row = 0; row < 4; row++) {
 	    for (int col = 0; col < 4; col++) {
-			    if(abidesRules(row, col, word, gameBoard, 0, visited)) {
-					    free(visited[0]);
-    					free(visited[1]);
-    					free(visited[2]);
-    					free(visited[3]);
-    				  return 1;
-
-			    }
-		   }
+		    if(abidesRules(row, col, word, gameBoard, 0, visited)) {
+				free(visited[0]);
+				free(visited[1]);
+				free(visited[2]);
+				free(visited[3]);
+			    return 1;
+			    
+			}
+		}
 	}
 
 	free(visited[0]);
@@ -108,7 +101,6 @@ int wordChecker(struct rolledDice **gameBoard, char *word) {
 	free(visited[3]);
 
 	return 0;
-
 }
 
 /*
@@ -116,24 +108,21 @@ int wordChecker(struct rolledDice **gameBoard, char *word) {
                return the letter at its location
                corresponding to a letter in a
                word that the user entered
-  Parameters: int i, int j, char **boggle
-  Return: boggle[i][j]
 */
 char testGetLetter(int i, int j, char **boggle) {
-
     return boggle[i][j];
 }
 
 /*
-  Description: This function will
-  Parameters: int i, int j, char *word, char **gameBoard, int subLen, int **visited
-  Return: 0 or 1
+  Description: This function will run a test to get a letter 
+  			   and see if at any point the program will produce
+			   a logic error
 */
 int testGetLetter(int i, int j, char *word, char **gameBoard, int subLen, int **visited) {
     int all_X[] = {0, -1, -1, -1, 0, 1, 1, 1};
-  	int all_Y[] = {1, 1, 0, -1, -1, -1, 0, 1};
-		int newX = 0;
-		int newY = 0;
+	int all_Y[] = {1, 1, 0, -1, -1, -1, 0, 1};
+	int newX = 0;
+	int newY = 0;
     int ans = (subLen == (strlen(word)-1));
     int result = 0;
     char currentLetter = word[subLen];
@@ -141,35 +130,34 @@ int testGetLetter(int i, int j, char *word, char **gameBoard, int subLen, int **
 
 	// use subLen as index to see what letter to find
   	if(subLen == (strlen(word)-1)) {
-  	    return 1;
+	    return 1;
   	}
 
-
     if(toupper(currentLetter) == 'Q' && toupper(word[subLen + 2]) == testGetLetter(i,j,gameBoard)) {
-	      findLetter = word[subLen + 3];
-				subLen++;
-				subLen++;
-				currentLetter = word[subLen];
-		}else {
-			  findLetter = word[subLen + 1];
-		}
+	    findLetter = word[subLen + 3];
+		subLen++;
+		subLen++;
+		currentLetter = word[subLen];
+	}
+	else {
+	    findLetter = word[subLen + 1];
+	}
 
-	  for(int adjCell = 0; adjCell < 8; adjCell++) {
-        int newX = i + all_X[adjCell];
-    		int newY = j + all_Y[adjCell];
-    		if((newX >= 0) && (newX < 4) && (newY >= 0) && (newY < 4) &&
-        toupper(findLetter) == testGetLetter(newX, newY, gameBoard) &&
-        !visited[newX][newY]) {
-    			    visited[newX][newY] = 1;
-    			    ++subLen;
-    			    result = testAbidesRules(newX, newX, word, gameBoard, subLen, visited);
-    				  if(result) {
-    				      return 1;
-    				  }else {
-    				      --subLen;
-    				  }
-    		   }
-        }
+	for(int adjCell = 0; adjCell < 8; adjCell++) {
+    	int newX = i + all_X[adjCell];
+		int newY = j + all_Y[adjCell];
+		if((newX >= 0) && (newX < 4) && (newY >= 0) && (newY < 4) && toupper(findLetter) == testGetLetter(newX, newY, gameBoard) && !visited[newX][newY]) {
+			visited[newX][newY] = 1;
+			++subLen;
+			result = testAbidesRules(newX, newX, word, gameBoard, subLen, visited);
+			if(result) {
+			    return 1;
+		  	}
+			else {
+				--subLen;
+			}
+	    }
+    }
   	return 0;
 }
 
